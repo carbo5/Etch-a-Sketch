8775db4431fb135e64df993b7bea03044c9accba
Bruno Carbonneau <accesshockeystats@gmail.com>
2020-12-01 12:51:42 -0500

    Adding a range input for the grid size

--- js/etch_a_sketch.js
+++ js/etch_a_sketch.js
@@ -8,19 +8,23 @@ var grid = document.querySelector('#red-container');
 
 function makeGrid(height, width, nbSquare){
 
+
+
     const redDiv = document.querySelector('#red-container');
+    redDiv.innerHTML = "";
+
     const remBorder = 2 * nbSquare;
 
    // let divWidth =  (width - 14 - remBorder) / nbSquare;
 
-   let divWidth =  width / nbSquare; //Largeur de chaque carré
+   let divWidth =  (width - 1) / nbSquare; //Largeur de chaque carré
 
     console.log(divWidth);
     
 
   //  let divHeight = (height - 14 - remBorder) / nbSquare;
 
-  let divHeight = height / nbSquare; //hauteur de chaque carré
+  let divHeight = (height - 0) / nbSquare; //hauteur de chaque carré
 
     console.log(divHeight);
     const totalSquare =  nbSquare * nbSquare;
@@ -30,8 +34,10 @@ function makeGrid(height, width, nbSquare){
         squareDiv = document.createElement('div');
         squareDiv.classList.add('gridDiv');
 
-        squareDiv.style.width = divWidth + 'px';
-        squareDiv.style.height = divHeight + 'px';
+        
+
+        squareDiv.style.width = (100 / nbSquare) + '%';
+        squareDiv.style.height = (100 / nbSquare) + '%';
        // squareDiv.style.border = '1px solid ligthgrey';
 
         
@@ -40,4 +46,17 @@ function makeGrid(height, width, nbSquare){
 
 }
 
-makeGrid(grid.clientHeight, grid.clientWidth, 50);
+/*managing range  */
+let range = document.querySelector("#rangeGrid");
+range.value = 16;
+rangeTexInput = document.querySelector('#rangeValue');
+rangeTexInput.value = range.value;
+
+range.addEventListener('change', (e) =>{
+    rangeTexInput = document.querySelector('#rangeValue');
+    rangeTexInput.value = range.value;
+    makeGrid(grid.clientHeight, grid.clientWidth, range.value);
+});
+
+
+makeGrid(grid.clientHeight, grid.clientWidth, 16);
